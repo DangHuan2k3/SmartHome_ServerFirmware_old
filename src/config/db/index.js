4a173155e5cfa77441318eb072a52674368e6af8
DangHuan2k3 <huan.bk.0412@gmail.com>
2024-03-12 22:39:51 +0700

    fix: route db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/SmartHome');
+        await mongoose.connect('mongodb+srv://huan:zmgGxoAE2beOZO2h@tugino.smpitlj.mongodb.net/ ');
 
         console.log('Connect successfully!!!');
     } catch (error) {
